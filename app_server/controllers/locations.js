d02b882b64376d29bb5a83a5fb116be1b650dca1
Saurabh Kaushik <kaushiksaurabh17@yahoo.com>
2017-01-28 19:18:24 +0530

    Getting MEAN Chapter 4 added view templates

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,17 +1,18 @@
 /* Get 'home' page */
 
 module.exports.homelist = function(req, res) {
-  res.render('index', { title: "Home" });  
+  res.render('locations-list', { title: "Home" });  
 };
 
 /* Get 'location info' page */
 
 module.exports.locationInfo = function(req, res) {
-  res.render('index', { title: "Location Info" });  
+  res.render('location-info', { title: "Location Info" });  
 };
 
 /* Get 'home' page */
 
 module.exports.addReview = function(req, res) {
-  res.render('index', { title: "Add Review" });  
+  res.render('location-review-form', { title: "Add Review" });  
 };
+
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,5 @@
 /* Get 'about' page */
 
 module.exports.about = function(req, res) {
-  res.render('index', { title: "About" });  
+  res.render('generic-text', { title: "About" });  
 };
